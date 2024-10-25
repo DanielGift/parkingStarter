8d015a3534c8b32b5b76e9f62a6ee58e4015c25d
Daniel Gift <daniel.gift@sbs.org>
2024-10-25 10:26:11 -0400

    Create parkingGarage.py

---
+++ parkingGarage.py
@@ -0,0 +1,30 @@
+def findSpot(garage, idealLoc, name):
+
+def removeCar(garage, name):
+
+def carIn(garage, name): #Optional
+
+def swapCars(garage, name1, name2):
+
+def countEmpty(garage):
+
+
+#Below are some of the print statements you might use.
+#You may need to rename variables, and you may need to reorder
+# or repeat some of these print statements. You will certainly
+# need to indent some of them to varying levels
+print("How big is the garage?")
+print("What do you want to do?")
+print("Which spot do you want to park near?")
+print("What is your car's name?")
+print("The garage is full, you cannot park!")
+print("You parked in spot "+ str(parked))
+print(name + " has been removed form the garage!")
+print(name + " is not in the garage!")
+print("What is the name of the first car you want to swap?")
+print("What is the name of the second car you want to swap?")
+print("The cars have been swapped in the garage!")
+print("At least one of your cars is not in the garage!")
+print("There are " + empty + " empty spaces left in the garage")
+print("That is not a valid command!")
+print("Goodbye!")
